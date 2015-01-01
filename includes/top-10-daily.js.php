8dee759d71f5a974d8219229211317b932085090
Ajay D'Souza <me@ajaydsouza.com>
2015-01-01 22:11:37 +0000

    Fixed includes

--- includes/top-10-daily.js.php
+++ includes/top-10-daily.js.php
@@ -11,7 +11,7 @@
 Header( "content-type: application/x-javascript" );
 
 if ( ! function_exists('add_action') ) {
-	$wp_root = '../../..';
+	$wp_root = '../../../..';
 	if ( file_exists($wp_root.'/wp-load.php') ) {
 		require_once( $wp_root . '/wp-load.php' );
 	} else {
