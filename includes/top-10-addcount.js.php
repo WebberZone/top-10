6f72d02554eef73ccc3460761e5a19db4966317e
Ajay D'Souza <me@ajaydsouza.com>
2015-01-03 00:59:39 +0000

    Fixed includes files; Updated copyright

--- includes/top-10-addcount.js.php
+++ includes/top-10-addcount.js.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * Add count to database.
+ * Update counts to database.
  *
  * @package   Top_Ten
  * @author    Ajay D'Souza <me@ajaydsouza.com>
  * @license   GPL-2.0+
  * @link      http://ajaydsouza.com
- * @copyright 2008-2014 Ajay D'Souza
+ * @copyright 2008-2015 Ajay D'Souza
  */
 Header( "content-type: application/x-javascript" );
 
@@ -33,22 +33,23 @@ global $wpdb;
 tptn_inc_count();
 function tptn_inc_count() {
 	global $wpdb;
-	$table_name = $wpdb->prefix . "top_ten";
-	$top_ten_daily = $wpdb->prefix . "top_ten_daily";
+	$table_name = $wpdb->base_prefix . "top_ten";
+	$top_ten_daily = $wpdb->base_prefix . "top_ten_daily";
 	$str = '';
 
 	$id = intval( $_GET['top_ten_id'] );
+	$blog_id = intval( $_GET['top_ten_blog_id'] );
 	$activate_counter = intval( $_GET['activate_counter'] );
+
 	if ( $id > 0 ) {
 		if ( ( 1 == $activate_counter ) || ( 11 == $activate_counter ) ) {
-			$tt = $wpdb->query( $wpdb->prepare( "INSERT INTO {$table_name} (postnumber, cntaccess) VALUES('%d', '1') ON DUPLICATE KEY UPDATE cntaccess= cntaccess+1 ", $id ) );
-			$str .= ( $tt === FALSE ) ? 'tte' : 'tt' . $tt;
+			$tt = $wpdb->query( $wpdb->prepare( "INSERT INTO {$table_name} (postnumber, cntaccess, blog_id) VALUES('%d', '1', '%d') ON DUPLICATE KEY UPDATE cntaccess= cntaccess+1 ", $id, $blog_id ) );
+			$str .= ( FALSE === $tt ) ? 'tte' : 'tt' . $tt;
 		}
 		if ( ( 10 == $activate_counter ) || ( 11 == $activate_counter ) ) {
 			$current_date = gmdate( 'Y-m-d H', current_time( 'timestamp', 1 ) );
-
-			$ttd = $wpdb->query( $wpdb->prepare( "INSERT INTO {$top_ten_daily} (postnumber, cntaccess, dp_date) VALUES('%d', '1', '%s' ) ON DUPLICATE KEY UPDATE cntaccess= cntaccess+1 ", $id, $current_date ) );
-			$str .= ($ttd === FALSE) ? ' ttde' : ' ttd' . $ttd;
+			$ttd = $wpdb->query( $wpdb->prepare("INSERT INTO {$top_ten_daily} (postnumber, cntaccess, dp_date, blog_id) VALUES('%d', '1', '%s', '%d' ) ON DUPLICATE KEY UPDATE cntaccess= cntaccess+1 ", $id, $current_date, $blog_id ) );
+			$str .= ( FALSE === $ttd ) ? ' ttde' : ' ttd' . $ttd;
 		}
 	}
 	echo '<!-- ' . $str . ' -->';
