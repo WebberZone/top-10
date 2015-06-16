f23e1b98cda694d2c99b569c1965d1cc76fcc8ae
Ajay D'Souza <me@ajaydsouza.com>
2015-06-16 22:44:05 +0100

    Change Filter from wp to parse_request

--- top-10.php
+++ top-10.php
@@ -14,7 +14,7 @@
  * Plugin Name:	Top 10
  * Plugin URI:	http://ajaydsouza.com/wordpress/plugins/top-10/
  * Description:	Count daily and total visits per post and display the most popular posts based on the number of views
- * Version: 	2.1.1-beta20150513
+ * Version: 	2.1.1-beta20150510
  * Author: 		Ajay D'Souza
  * Author URI: 	http://ajaydsouza.com
  * Text Domain:	tptn
@@ -281,7 +281,7 @@ function tptn_parse_request( $wp ) {
 		return;
 	}
 }
-add_action( 'wp', 'tptn_parse_request' );
+add_action( 'parse_request', 'tptn_parse_request' );
 
 
 /**
