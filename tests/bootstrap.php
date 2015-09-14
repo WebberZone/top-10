f9221552ede1267ee9667f4741c86a43c30bf736
Ajay D'Souza <me@ajaydsouza.com>
2015-09-14 17:41:15 +0100

    Updated tests bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,9 @@
 <?php
 
+$_SERVER['SERVER_PROTOCOL'] = 'HTTP/1.1';
+$_SERVER['SERVER_NAME'] = '';
+$PHP_SELF = $GLOBALS['PHP_SELF'] = $_SERVER['PHP_SELF'] = '/index.php';
+
 $_tests_dir = getenv('WP_TESTS_DIR');
 if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
 
@@ -11,4 +15,8 @@ function _manually_load_plugin() {
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
 require $_tests_dir . '/includes/bootstrap.php';
+
+activate_plugin( 'top-10/top-10.php' );
+
+echo "Installing Top 10...\n";
 
