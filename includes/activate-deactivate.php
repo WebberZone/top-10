9f75a50df1972e9d0c285fdab78633856a09ba49
Ajay D'Souza <me@ajaydsouza.com>
2016-03-23 21:12:35 +0000

    Split top-10.php

---
+++ includes/activate-deactivate.php
@@ -0,0 +1,179 @@
+<?php
+/**
+ * Functions run on activation / deactivation.
+ *
+ * @package Top_Ten
+ */
+
+
+/**
+ * Fired when the plugin is Network Activated.
+ *
+ * @since 1.9.10.1
+ *
+ * @param    boolean $network_wide    True if WPMU superadmin uses
+ *                                    "Network Activate" action, false if
+ *                                    WPMU is disabled or plugin is
+ *                                    activated on an individual blog.
+ */
+function tptn_activation_hook( $network_wide ) {
+	global $wpdb;
+
+	if ( is_multisite() && $network_wide ) {
+
+		// Get all blogs in the network and activate plugin on each one
+		$blog_ids = $wpdb->get_col( "
+        	SELECT blog_id FROM $wpdb->blogs
+			WHERE archived = '0' AND spam = '0' AND deleted = '0'
+		" );
+		foreach ( $blog_ids as $blog_id ) {
+			switch_to_blog( $blog_id );
+			tptn_single_activate();
+		}
+
+		// Switch back to the current blog
+		restore_current_blog();
+
+	} else {
+		tptn_single_activate();
+	}
+}
+register_activation_hook( TOP_TEN_PLUGIN_FILE, 'tptn_activation_hook' );
+
+
+/**
+ * Fired for each blog when the plugin is activated.
+ *
+ * @since 2.0.0
+ */
+function tptn_single_activate() {
+	global $wpdb, $tptn_db_version;
+
+	$tptn_settings = tptn_read_options();
+
+	$table_name = $wpdb->base_prefix . 'top_ten';
+	$table_name_daily = $wpdb->base_prefix . 'top_ten_daily';
+
+	if ( $wpdb->get_var( "show tables like '$table_name'" ) != $table_name ) {
+
+		$sql = 'CREATE TABLE ' . $table_name . " (
+			postnumber bigint(20) NOT NULL,
+			cntaccess bigint(20) NOT NULL,
+			blog_id bigint(20) NOT NULL DEFAULT '1',
+			PRIMARY KEY  (postnumber, blog_id)
+			);";
+
+		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+		dbDelta( $sql );
+
+		add_site_option( 'tptn_db_version', $tptn_db_version );
+	}
+
+	if ( $wpdb->get_var( "show tables like '$table_name_daily'" ) != $table_name_daily ) {
+
+		$sql = 'CREATE TABLE ' . $table_name_daily . " (
+			postnumber bigint(20) NOT NULL,
+			cntaccess bigint(20) NOT NULL,
+			dp_date DATETIME NOT NULL,
+			blog_id bigint(20) NOT NULL DEFAULT '1',
+			PRIMARY KEY  (postnumber, dp_date, blog_id)
+		);";
+
+		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+		dbDelta( $sql );
+
+		add_site_option( 'tptn_db_version', $tptn_db_version );
+	}
+
+	// Upgrade table code
+	$installed_ver = get_site_option( 'tptn_db_version' );
+
+	if ( $installed_ver != $tptn_db_version ) {
+
+		$wpdb->hide_errors();
+
+		switch ( $installed_ver ) {
+
+			case '4.0':
+			case 4.0:
+				$wpdb->query( 'ALTER TABLE ' . $table_name . " CHANGE blog_id blog_id bigint(20) NOT NULL DEFAULT '1'" );
+				$wpdb->query( 'ALTER TABLE ' . $table_name_daily . " CHANGE blog_id blog_id bigint(20) NOT NULL DEFAULT '1'" );
+				break;
+
+			default:
+
+				$wpdb->query( 'ALTER TABLE ' . $table_name . ' MODIFY postnumber bigint(20) ' );
+				$wpdb->query( 'ALTER TABLE ' . $table_name_daily . ' MODIFY postnumber bigint(20) ' );
+				$wpdb->query( 'ALTER TABLE ' . $table_name . ' MODIFY cntaccess bigint(20) ' );
+				$wpdb->query( 'ALTER TABLE ' . $table_name_daily . ' MODIFY cntaccess bigint(20) ' );
+				$wpdb->query( 'ALTER TABLE ' . $table_name_daily . ' MODIFY dp_date DATETIME ' );
+				$wpdb->query( 'ALTER TABLE ' . $table_name . ' DROP PRIMARY KEY, ADD PRIMARY KEY(postnumber, blog_id) ' );
+				$wpdb->query( 'ALTER TABLE ' . $table_name_daily . ' DROP PRIMARY KEY, ADD PRIMARY KEY(postnumber, dp_date, blog_id) ' );
+				$wpdb->query( 'ALTER TABLE ' . $table_name . " ADD blog_id bigint(20) NOT NULL DEFAULT '1'" );
+				$wpdb->query( 'ALTER TABLE ' . $table_name_daily . " ADD blog_id bigint(20) NOT NULL DEFAULT '1'" );
+				$wpdb->query( 'UPDATE ' . $table_name . ' SET blog_id = 1 WHERE blog_id = 0 ' );
+				$wpdb->query( 'UPDATE ' . $table_name_daily . ' SET blog_id = 1 WHERE blog_id = 0 ' );
+
+		}
+
+		$wpdb->show_errors();
+
+		update_site_option( 'tptn_db_version', $tptn_db_version );
+	}
+
+}
+
+
+/**
+ * Fired when a new site is activated with a WPMU environment.
+ *
+ * @since 2.0.0
+ *
+ * @param    int $blog_id    ID of the new blog.
+ */
+function tptn_activate_new_site( $blog_id ) {
+
+	if ( 1 !== did_action( 'wpmu_new_blog' ) ) {
+		return;
+	}
+
+	switch_to_blog( $blog_id );
+	tptn_single_activate();
+	restore_current_blog();
+
+}
+add_action( 'wpmu_new_blog', 'tptn_activate_new_site' );
+
+
+/**
+ * Fired when a site is deleted in a WPMU environment.
+ *
+ * @since 2.0.0
+ *
+ * @param    array $tables    Tables in the blog.
+ */
+function tptn_on_delete_blog( $tables ) {
+	global $wpdb;
+
+	$tables[] = $wpdb->prefix . 'top_ten';
+	$tables[] = $wpdb->prefix . 'top_ten_daily';
+
+	return $tables;
+}
+add_filter( 'wpmu_drop_tables', 'tptn_on_delete_blog' );
+
+
+/**
+ * Function to call install function if needed.
+ *
+ * @since	1.9
+ */
+function tptn_update_db_check() {
+	global $tptn_db_version, $network_wide;
+
+	if ( get_site_option( 'tptn_db_version' ) != $tptn_db_version ) {
+		tptn_activation_hook( $network_wide );
+	}
+}
+add_action( 'plugins_loaded', 'tptn_update_db_check' );
+
---
+++ includes/l10n.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Language functions
+ *
+ * @package Top_Ten
+ */
+
+
+/**
+ * Function to load translation files.
+ *
+ * @since	1.9.10.1
+ */
+function tptn_lang_init() {
+	load_plugin_textdomain( 'top-10', false, dirname( plugin_basename( TOP_TEN_PLUGIN_FILE ) ) . '/languages/' );
+}
+add_action( 'plugins_loaded', 'tptn_lang_init' );
+
+
+/**
+ * Fetch the post of the correct language.
+ *
+ * @since	2.1.0
+ *
+ * @param	$post_id	Post ID
+ */
+function tptn_object_id_cur_lang( $post_id ) {
+
+	$return_original_if_missing = false;
+
+	/**
+	 * Filter to modify if the original language ID is returned.
+	 *
+	 * @since	2.2.3
+	 *
+	 * @param	bool	$return_original_if_missing
+	 * @param	int	$post_id	Post ID
+	 */
+	$return_original_if_missing = apply_filters( 'tptn_wpml_return_original', $return_original_if_missing, $post_id );
+
+	if ( function_exists( 'pll_get_post' ) ) {
+		$post_id = pll_get_post( $post_id );
+	} elseif ( function_exists( 'wpml_object_id_filter' ) ) {
+		$post_id = wpml_object_id_filter( $post_id, 'any', $return_original_if_missing );
+	} elseif ( function_exists( 'icl_object_id' ) ) {
+		$post_id = icl_object_id( $post_id, 'any', $return_original_if_missing );
+	}
+
+	/**
+	 * Filters object ID for current language (WPML).
+	 *
+	 * @since	2.1.0
+	 *
+	 * @param	int	$post_id	Post ID
+	 */
+	return apply_filters( 'tptn_object_id_cur_lang', $post_id );
+}
+
+
