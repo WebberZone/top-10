5fea10e923551ba8e5f2c47c93698b428cd4abf9
Ajay D'Souza <me@ajaydsouza.com>
2016-03-25 11:06:13 +0000

    Tagged 2.3.0

--- top-10.php
+++ top-10.php
@@ -14,7 +14,7 @@
  * Plugin Name:	Top 10
  * Plugin URI:	https://webberzone.com/plugins/top-10/
  * Description:	Count daily and total visits per post and display the most popular posts based on the number of views
- * Version: 	2.3.0-beta20160324
+ * Version: 	2.3.0
  * Author: 		Ajay D'Souza
  * Author URI: 	https://webberzone.com
  * License: 	GPL-2.0+
