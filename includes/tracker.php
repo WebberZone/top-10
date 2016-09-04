504ba9d057470906c0c4c708245318de12179dee
Ajay DSouza <me@ajaydsouza.com>
2016-09-04 17:09:42 +0100

    New option for type of tracker

--- includes/tracker.php
+++ includes/tracker.php
@@ -39,10 +39,17 @@ function tptn_enqueue_scripts() {
 			$activate_counter = $tptn_settings['activate_overall'] ? 1 : 0;		// It's 1 if we're updating the overall count.
 			$activate_counter = $activate_counter + ( $tptn_settings['activate_daily'] ? 10 : 0 );	// It's 10 if we're updating the daily count.
 
-			$home_url = home_url( '/' );
+			if ( 'query_based' === $tptn_settings['tracker_type'] ) {
+				$home_url = home_url( '/' );
+			} else {
+				$home_url = admin_url( 'admin-ajax.php' );
+			}
 
 			/**
-			 * Filter the script URL of the counter.
+			 * Filter the URL of the tracker.
+			 *
+			 * Other tracker types can override the URL processed by the jQuery.post request
+			 * The corresponding tracker can use the below variables or append their own to $ajax_tptn_tracker
 			 *
 			 * @since	2.0
 			 */
@@ -51,18 +58,25 @@ function tptn_enqueue_scripts() {
 			// Strip any query strings since we don't need them.
 			$home_url = strtok( $home_url, '?' );
 
-			// $home_url = admin_url( 'admin-ajax.php' );
+			$ajax_tptn_tracker = array(
+				'ajax_url' => $home_url,
+				'top_ten_nonce' => wp_create_nonce( 'tptn-tracker-nonce' ),
+				'top_ten_id' => $id,
+				'top_ten_blog_id' => $blog_id,
+				'activate_counter' => $activate_counter,
+				'tptn_rnd' => wp_rand( 1, time() ),
+			);
+
+			/**
+			 * Filter the localize script arguments for the Top 10 tracker.
+			 *
+			 * @since 2.4.0
+			 */
+			$ajax_tptn_tracker = apply_filters( 'tptn_tracker_script_args', $ajax_tptn_tracker );
+
 			wp_enqueue_script( 'tptn_tracker', plugins_url( 'includes/js/top-10-tracker.js', TOP_TEN_PLUGIN_FILE ), array( 'jquery' ) );
 
-			wp_localize_script( 'tptn_tracker', 'ajax_tptn_tracker', array(
-					'ajax_url' => $home_url,
-					'top_ten_nonce' => wp_create_nonce( 'tptn-tracker-nonce' ),
-					'top_ten_id' => $id,
-					'top_ten_blog_id' => $blog_id,
-					'activate_counter' => $activate_counter,
-					'tptn_rnd' => wp_rand( 1, time() ),
-				)
-			);
+			wp_localize_script( 'tptn_tracker', 'ajax_tptn_tracker', $ajax_tptn_tracker );
 
 		}
 	}
@@ -221,102 +235,32 @@ add_action( 'wp_ajax_tptn_tracker', 'tptn_tracker_parser' );
 
 
 /**
- * Function to update the post views for the current post. Filters `the_content`.
+ * Function returns the different types of trackers.
  *
- * @since	1.0
- *
- * @deprecated 2.4.0
- *
- * @param	string $content Post content.
- * @return	string	Filtered content
+ * @since 2.4.0
+ * @return array Tracker types.
  */
-function tptn_add_viewed_count( $content = '' ) {
-	global $post, $tptn_settings;
+function tptn_get_tracker_types() {
 
-	$home_url = home_url( '/' );
+	$trackers = array(
+		array(
+			'id' => 'query_based',
+			'name' => __( 'Query variable based', 'top-10' ),
+			'description' => __( 'Uses query variables to record visits', 'top-10' ),
+		),
+		array(
+			'id' => 'ajaxurl',
+			'name' => __( 'Ajaxurl based', 'top-10' ),
+			'description' => __( 'Uses admin-ajax.php which is inbuilt within WordPress to process the tracker', 'top-10' ),
+		),
+	);
 
 	/**
-	 * Filter the script URL of the counter.
+	 * Filter the array containing the types of trackers to add your own.
 	 *
-	 * @since	2.0
+	 * @since 2.4.0
+	 *
+	 * @param string $trackers Different trackers.
 	 */
-	$home_url = apply_filters( 'tptn_add_counter_script_url', $home_url );
-
-	// Strip any query strings since we don't need them.
-	$home_url = strtok( $home_url, '?' );
-
-	if ( is_singular() && 'draft' !== $post->post_status ) {
-
-		$current_user = wp_get_current_user();	// Let's get the current user
-		$post_author = ( $current_user->ID == $post->post_author ) ? true : false;	// Is the current user the post author?
-		$current_user_admin = ( current_user_can( 'manage_options' ) ) ? true : false;	// Is the current user an admin?
-		$current_user_editor = ( ( current_user_can( 'edit_others_posts' ) ) && ( ! current_user_can( 'manage_options' ) ) ) ? true : false;	// Is the current user an editor?
-
-		$include_code = true;
-		if ( ( $post_author ) && ( ! $tptn_settings['track_authors'] ) ) {
-			$include_code = false;
-		}
-		if ( ( $current_user_admin ) && ( ! $tptn_settings['track_admins'] ) ) {
-			$include_code = false;
-		}
-		if ( ( $current_user_editor ) && ( ! $tptn_settings['track_editors'] ) ) {
-			$include_code = false;
-		}
-
-		if ( $include_code ) {
-
-			$output = '';
-			$id = intval( $post->ID );
-			$blog_id = get_current_blog_id();
-			$activate_counter = $tptn_settings['activate_overall'] ? 1 : 0;		// It's 1 if we're updating the overall count.
-			$activate_counter = $activate_counter + ( $tptn_settings['activate_daily'] ? 10 : 0 );	// It's 10 if we're updating the daily count.
-
-			if ( $activate_counter > 0 ) {
-					$output = '
-					<script type="text/javascript"> jQuery(document).ready(function() {
-						jQuery.ajax({
-							url: "' . $home_url . '",
-							data: {
-								top_ten_id: ' . $id . ',
-								top_ten_blog_id: ' . $blog_id . ',
-								activate_counter: ' . $activate_counter . ',
-								top10_rnd: (new Date()).getTime() + "-" + Math.floor(Math.random() * 100000)
-							}
-						});
-					});</script>';
-			}
-
-			/**
-			 * Filter the counter script
-			 *
-			 * @since	1.9.8.5
-			 *
-			 * @param	string	$output	Counter script code
-			 */
-			$output = apply_filters( 'tptn_viewed_count', $output );
-
-			echo $output;
-		}
-	}
+	return apply_filters( 'tptn_get_tracker_types', $trackers );
 }
-
-
-/**
- * Add tracker code.
- *
- * @since 2.3.0
- *
- * @deprecated 2.4.0
- *
- * @param bool $echo Echo the code or return it.
- * @return string|void
- */
-function tptn_add_tracker( $echo = true ) {
-
-	if ( $echo ) {
-		echo tptn_add_viewed_count( '' );
-	} else {
-		return tptn_add_viewed_count( '' );
-	}
-}
-
