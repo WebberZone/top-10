f65b6c48d9c2937aa62fd0257a0843cc71842e1d
Ajay DSouza <me@ajaydsouza.com>
2016-09-04 13:16:09 +0100

    Deprecated footer action. Now handled by .js file

--- includes/js/top-10-tracker.js
+++ includes/js/top-10-tracker.js
@@ -6,7 +6,5 @@ jQuery( document ).ready(function($) {
 		'top_ten_blog_id': ajax_tptn_tracker.top_ten_blog_id,
 		'activate_counter': ajax_tptn_tracker.activate_counter
 	};
-	jQuery.post(ajax_tptn_tracker.ajax_url, data, function(response) {
-		console.log( '<!--' + response + '-->' );
-	});
+	jQuery.post( ajax_tptn_tracker.ajax_url, data );
 });
