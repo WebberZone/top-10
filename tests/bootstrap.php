530bde25e2ab04314cfb60db58300550b9f3541e
Ajay DSouza <me@ajaydsouza.com>
2017-08-12 22:09:14 +0100

    Updated testing files

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,7 +10,7 @@ if ( ! $_tests_dir ) { $_tests_dir = '/tmp/wordpress-tests-lib'; }
 require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin() {
-	require dirname( __FILE__ ) . '/../top-10.php';
+	require dirname( dirname( __FILE__ ) ) . '/top-10.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
