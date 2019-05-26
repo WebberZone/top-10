3cbb98dee896b645c60317bb65519ed2356d35a8
Ajay DSouza <me@ajaydsouza.com>
2019-05-27 00:06:47 +0100

    Import/Export settings

--- includes/admin/import-export.php
+++ includes/admin/import-export.php
@@ -24,9 +24,14 @@ if ( ! defined( 'WPINC' ) ) {
  */
 function tptn_exim_page() {
 
-	/* Truncate overall posts table */
+	/* Message for successful file import */
 	if ( isset( $_GET['file_import'] ) && 'success' === $_GET['file_import'] ) { // phpcs:ignore WordPress.Security.NonceVerification.Recommended
 		add_settings_error( 'tptn-notices', '', esc_html__( 'Data has been imported into the table', 'top-10' ), 'updated' );
+	}
+
+	/* Message for successful file import */
+	if ( isset( $_GET['settings_import'] ) && 'success' === $_GET['settings_import'] ) { // phpcs:ignore WordPress.Security.NonceVerification.Recommended
+		add_settings_error( 'tptn-notices', '', esc_html__( 'Settings have been imported successfully', 'top-10' ), 'updated' );
 	}
 
 	ob_start();
@@ -40,7 +45,37 @@ function tptn_exim_page() {
 		<div id="post-body" class="metabox-holder columns-2">
 		<div id="post-body-content">
 
-			<form method="post" >
+			<form method="post">
+
+				<h2 style="padding-left:0px"><?php esc_html_e( 'Export/Import settings', 'top-10' ); ?></h2>
+				<p class="description">
+					<?php esc_html_e( 'Export the plugin settings for this site as a .json file. This allows you to easily import the configuration into another site.', 'top-10' ); ?>
+				</p>
+				<p><input type="hidden" name="tptn_action" value="export_settings" /></p>
+				<p>
+					<?php submit_button( esc_html__( 'Export Settings', 'top-10' ), 'primary', 'tptn_export_settings', false ); ?>
+				</p>
+
+				<?php wp_nonce_field( 'tptn_export_setting_nonce', 'tptn_export_setting_nonce' ); ?>
+			</form>
+
+			<form method="post" enctype="multipart/form-data">
+
+				<p class="description">
+					<?php esc_html_e( 'Import the plugin settings from a .json file. This file can be obtained by exporting the settings on another site using the form above.', 'top-10' ); ?>
+				</p>
+				<p>
+					<input type="file" name="import_settings_file" />
+				</p>
+				<p>
+					<?php submit_button( esc_html__( 'Import Settings', 'top-10' ), 'primary', 'tptn_import_settings', false ); ?>
+				</p>
+
+				<input type="hidden" name="tptn_action" value="import_settings" />
+				<?php wp_nonce_field( 'tptn_import_settings_nonce', 'tptn_import_settings_nonce' ); ?>
+			</form>
+
+			<form method="post">
 
 				<h2 style="padding-left:0px"><?php esc_html_e( 'Export tables', 'top-10' ); ?></h2>
 				<p class="description">
@@ -70,7 +105,6 @@ function tptn_exim_page() {
 					<input type="file" name="import_file" />
 				</p>
 				<p>
-					<input type="hidden" name="tptn_action" value="import_settings" />
 					<?php submit_button( esc_html__( 'Import Overall CSV', 'top-10' ), 'primary', 'tptn_import_total', false ); ?>
 				</p>
 
@@ -79,10 +113,10 @@ function tptn_exim_page() {
 					<input type="file" name="import_file_daily" />
 				</p>
 				<p>
-					<input type="hidden" name="tptn_action" value="import_settings" />
 					<?php submit_button( esc_html__( 'Import Daily CSV', 'top-10' ), 'primary', 'tptn_import_daily', false ); ?>
 				</p>
 
+				<input type="hidden" name="tptn_action" value="import_settings" />
 				<?php wp_nonce_field( 'tptn_import_nonce', 'tptn_import_nonce' ); ?>
 			</form>
 
@@ -233,7 +267,7 @@ function tptn_import_tables() {
 	$import_file = isset( $_FILES[ $filename ]['tmp_name'] ) ? ( wp_unslash( $_FILES[ $filename ]['tmp_name'] ) ) : ''; // phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
 
 	if ( empty( $import_file ) ) {
-		wp_die( esc_html__( 'Please upload a file with a valid name (no special characters) to import', 'top-10' ) );
+		wp_die( esc_html__( 'Please upload a file to import', 'top-10' ) );
 	}
 
 	// Truncate the table before import.
@@ -273,3 +307,87 @@ function tptn_import_tables() {
 
 }
 add_action( 'admin_init', 'tptn_import_tables' );
+
+/**
+ * Process a settings export that generates a .json file of the shop settings
+ *
+ * @since 2.7.0
+ */
+function tptn_process_settings_export() {
+
+	if ( empty( $_POST['tptn_action'] ) || 'export_settings' !== $_POST['tptn_action'] ) {
+		return;
+	}
+
+	if ( isset( $_POST['tptn_export_settings_nonce'] ) && ! wp_verify_nonce( sanitize_key( $_POST['tptn_export_settings_nonce'] ), 'tptn_export_settings_nonce' ) ) {
+		return;
+	}
+
+	if ( ! current_user_can( 'manage_options' ) ) {
+		return;
+	}
+
+	$settings = get_option( 'tptn_settings' );
+
+	ignore_user_abort( true );
+
+	nocache_headers();
+	header( 'Content-Type: application/json; charset=utf-8' );
+	header( 'Content-Disposition: attachment; filename=tptn-settings-export-' . date( 'm-d-Y' ) . '.json' );
+	header( 'Expires: 0' );
+
+	echo wp_json_encode( $settings );
+	exit;
+}
+add_action( 'admin_init', 'tptn_process_settings_export' );
+
+/**
+ * Process a settings import from a json file
+ *
+ * @since 2.7.0
+ */
+function tptn_process_settings_import() {
+
+	if ( empty( $_POST['tptn_action'] ) || 'import_settings' !== $_POST['tptn_action'] ) {
+		return;
+	}
+
+	if ( isset( $_POST['tptn_import_settings_nonce'] ) && ! wp_verify_nonce( sanitize_key( $_POST['tptn_import_settings_nonce'] ), 'tptn_import_settings_nonce' ) ) {
+		return;
+	}
+
+	if ( ! current_user_can( 'manage_options' ) ) {
+		return;
+	}
+
+	$filename  = 'import_settings_file';
+	$extension = isset( $_FILES[ $filename ]['name'] ) ? end( explode( '.', sanitize_file_name( wp_unslash( $_FILES[ $filename ]['name'] ) ) ) ) : '';
+
+	if ( 'json' !== $extension ) {
+		wp_die( esc_html__( 'Please upload a valid .json file', 'top-10' ) );
+	}
+
+	$import_file = isset( $_FILES[ $filename ]['tmp_name'] ) ? ( wp_unslash( $_FILES[ $filename ]['tmp_name'] ) ) : ''; // phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
+
+	if ( empty( $import_file ) ) {
+		wp_die( esc_html__( 'Please upload a file to import', 'top-10' ) );
+	}
+
+	// Retrieve the settings from the file and convert the json object to an array.
+	$settings = (array) json_decode( file_get_contents( $import_file ), true ); // phpcs:ignore WordPress.WP.AlternativeFunctions.file_get_contents_file_get_contents
+
+	update_option( 'tptn_settings', $settings );
+
+	wp_safe_redirect(
+		add_query_arg(
+			array(
+				'page'            => 'tptn_exim_page',
+				'settings_import' => 'success',
+			),
+			admin_url( 'admin.php' )
+		)
+	);
+	exit;
+
+}
+add_action( 'admin_init', 'tptn_process_settings_import' );
