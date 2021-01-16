6ef093a946a70a332d7fcd565f260b6828f61109
Ajay DSouza <me@ajaydsouza.com>
2021-01-16 20:13:46 +0000

    Exclude the Front page and Posts page
    Fixes #107

--- includes/modules/exclusions.php
+++ includes/modules/exclusions.php
@@ -6,7 +6,7 @@
  */
 
 /**
- * Function to filter exclude post IDs.
+ * Add additional post IDs to exclude. Filters `tptn_exclude_post_ids`.
  *
  * @since   2.2.0
  *
@@ -18,6 +18,7 @@ function tptn_exclude_post_ids( $exclude_post_ids ) {
 
 	$exclude_post_ids = (array) $exclude_post_ids;
 
+	// Find all posts that have `exclude_this_post` set.
 	$tptn_post_metas = $wpdb->get_results( "SELECT post_id, meta_value FROM {$wpdb->postmeta} WHERE `meta_key` = 'tptn_post_meta'", ARRAY_A ); // phpcs:ignore WordPress.DB.DirectDatabaseQuery.DirectQuery, WordPress.DB.DirectDatabaseQuery.NoCaching, WordPress.DB.PreparedSQL.InterpolatedNotPrepared
 
 	foreach ( $tptn_post_metas as $tptn_post_meta ) {
@@ -27,8 +28,20 @@ function tptn_exclude_post_ids( $exclude_post_ids ) {
 			$exclude_post_ids[] = $tptn_post_meta['post_id'];
 		}
 	}
-	return $exclude_post_ids;
 
+	// Exclude page_on_front and page_for_posts.
+	if ( 'page' === get_option( 'show_on_front' ) && tptn_get_option( 'exclude_front' ) ) {
+		$page_on_front  = get_option( 'page_on_front' );
+		$page_for_posts = get_option( 'page_for_posts' );
+		if ( $page_on_front > 0 ) {
+			$exclude_post_ids[] = $page_on_front;
+		}
+		if ( $page_for_posts > 0 ) {
+			$exclude_post_ids[] = $page_for_posts;
+		}
+	}
+
+	return $exclude_post_ids;
 }
 add_filter( 'tptn_exclude_post_ids', 'tptn_exclude_post_ids' );
 
