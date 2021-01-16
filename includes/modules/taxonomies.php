5576fccfeb669782a8d2f6071f06189c730f30d8
Ajay DSouza <me@ajaydsouza.com>
2021-01-16 12:09:29 +0000

    Delete unnecessary joins and groupby
    Fixes #105

--- includes/modules/taxonomies.php
+++ includes/modules/taxonomies.php
@@ -11,31 +11,6 @@ if ( ! defined( 'WPINC' ) ) {
 }
 
 /**
- * Filter JOIN clause of tptn query to add taxonomy tables.
- *
- * @since 2.2.0
- *
- * @param   mixed $join Join clause.
- * @return  string  Filtered JOIN clause
- */
-function tptn_exclude_categories_join( $join ) {
-	global $wpdb, $tptn_settings;
-
-	if ( '' !== tptn_get_option( 'exclude_categories' ) ) {
-
-		$sql  = $join;
-		$sql .= " LEFT JOIN $wpdb->term_relationships AS excat_tr ON ($wpdb->posts.ID = excat_tr.object_id) ";
-		$sql .= " LEFT JOIN $wpdb->term_taxonomy AS excat_tt ON (excat_tr.term_taxonomy_id = excat_tt.term_taxonomy_id) ";
-
-		return $sql;
-	} else {
-		return $join;
-	}
-}
-add_filter( 'tptn_posts_join', 'tptn_exclude_categories_join' );
-
-
-/**
  * Filter WHERE clause of tptn query to exclude posts belonging to certain categories.
  *
  * @since 2.2.0
@@ -65,29 +40,4 @@ function tptn_exclude_categories_where( $where ) {
 
 }
 add_filter( 'tptn_posts_where', 'tptn_exclude_categories_where' );
-
-
-/**
- * Filter GROUP BY clause of tptn query to exclude posts belonging to certain categories.
- *
- * @since 2.3.0
- *
- * @param   mixed $groupby GROUP BY clause.
- * @return  string  Filtered GROUP BY clause
- */
-function tptn_exclude_categories_groupby( $groupby ) {
-	global $tptn_settings;
-
-	if ( '' !== tptn_get_option( 'exclude_categories' ) && '' !== $groupby ) {
-
-		$sql  = $groupby;
-		$sql .= ', excat_tt.term_taxonomy_id ';
-
-		return $sql;
-	} else {
-		return $groupby;
-	}
-}
-add_filter( 'tptn_posts_groupby', 'tptn_exclude_categories_groupby' );
-
 
