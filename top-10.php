05982f37220c52a8788cb356c97157479eef80ee
Ajay DSouza <me@ajaydsouza.com>
2022-04-08 15:24:10 +0100

    Tagged v3.1.1
    Fixed parse error

--- top-10.php
+++ top-10.php
@@ -14,7 +14,7 @@
  * Plugin Name: Top 10
  * Plugin URI:  https://webberzone.com/plugins/top-10/
  * Description: Count daily and total visits per post and display the most popular posts based on the number of views
- * Version:     3.1.0
+ * Version:     3.1.1
  * Author:      Ajay D'Souza
  * Author URI:  https://webberzone.com
  * License:     GPL-2.0+
