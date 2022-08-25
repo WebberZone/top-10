ee16a73bba615a75934d3f7dbe43a12cf7205e57
Ajay DSouza <me@ajaydsouza.com>
2022-08-25 20:28:53 +0100

    Updated Top 10 Block
    Fixes #121
    
    Instead of Placeholder, the urQueryArgs is passed a site locale

--- includes/blocks/popular-posts/build/index.asset.php
+++ includes/blocks/popular-posts/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'b737d55f4dc2398006195bc932404a85');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '728c056e7b49267a5403');
