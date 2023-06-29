e25eb3432dc4110afd5d7839a4f0bea04bc55a59
Ajay D'Souza <ajaydsouza@users.noreply.github.com>
2023-06-29 19:59:01 +0100

    Update bootstrap.php

--- phpunit/bootstrap.php
+++ phpunit/bootstrap.php
@@ -6,21 +6,6 @@ if ( class_exists( '\Yoast\PHPUnitPolyfills\Autoload' ) === false ) {
     require_once 'vendor/yoast/phpunit-polyfills/phpunitpolyfills-autoload.php';
 }
 
-$versionRequirement = '1.0.1';
-if ( defined( '\Yoast\PHPUnitPolyfills\Autoload::VERSION' ) === false
-    || version_compare( \Yoast\PHPUnitPolyfills\Autoload::VERSION, $versionRequirement, '<' )
-) {
-    echo 'Error: Version mismatch detected for the PHPUnit Polyfills.',
-        ' Please ensure that PHPUnit Polyfills ', $versionRequirement,
-        ' or higher is loaded.', PHP_EOL;
-    exit(1);
-} else {
-    echo 'Error: Please run `composer update -W` before running the tests.' . PHP_EOL;
-    echo 'You can still use a PHPUnit phar to run them,',
-        ' but the dependencies do need to be installed.', PHP_EOL;
-    exit(1);
-}
-
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 
 // Check if we're installed in a src checkout.
