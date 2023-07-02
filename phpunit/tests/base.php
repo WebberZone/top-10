30b47a4329990cf8d6fc1fab72bcb48a6aad14fe
Ajay D'Souza <ajaydsouza@users.noreply.github.com>
2023-07-02 17:57:04 +0100

    Update base.php

--- phpunit/tests/base.php
+++ phpunit/tests/base.php
@@ -1,6 +1,6 @@
 <?php
 
-class Sample_Test extends WP_UnitTestCase {
+class Base extends WP_UnitTestCase {
 	public function test_post_type_exists() {
 		// The full application and it's functions are loaded.
 		$this->assertTrue( post_type_exists( 'post' ) );
