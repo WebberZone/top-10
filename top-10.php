560e640939fb9a674919287824ff21c46c5a51a3
Ajay D'Souza <ajaydsouza@users.noreply.github.com>
2023-10-28 22:33:19 +0100

    Tagged 3.3.3 RC1

--- top-10.php
+++ top-10.php
@@ -14,7 +14,7 @@
  * Plugin Name: Top 10
  * Plugin URI:  https://webberzone.com/plugins/top-10/
  * Description: Count daily and total visits per post and display the most popular posts based on the number of views
- * Version:     3.3.3-beta1
+ * Version:     3.3.3-RC1
  * Author:      WebberZone
  * Author URI:  https://webberzone.com
  * License:     GPL-2.0+
