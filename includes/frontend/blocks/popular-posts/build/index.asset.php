1d041490858f90455614085e9e91e91f79f612e4
Ajay D'Souza <ajaydsouza@users.noreply.github.com>
2023-11-02 21:13:06 +0000

    Updated block
    Fixes #154

--- includes/frontend/blocks/popular-posts/build/index.asset.php
+++ includes/frontend/blocks/popular-posts/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '0000fdb850ea5b1ec654');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => 'adff22f37f9d1ccaae4d');
