a62832df4ac97e085ae478fe4e3d9a8cffe4b45b
Ajay D'Souza <ajaydsouza@users.noreply.github.com>
2025-12-31 15:56:59 +0530

    Update copyright year to 2026

--- top-10.php
+++ top-10.php
@@ -8,7 +8,7 @@
  * @author    Ajay D'Souza
  * @license   GPL-2.0+
  * @link      https://webberzone.com
- * @copyright 2008-2025 Ajay D'Souza
+ * @copyright 2008-2026 Ajay D'Souza
  *
  * @wordpress-plugin
  * Plugin Name: Top 10
