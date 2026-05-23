d909f75741e0053aba2797b5e912140bffaa1ced
Ajay D'Souza <ajaydsouza@users.noreply.github.com>
2026-05-23 14:42:23 +0400

    Update tom-select.complete.min.js to single-line minified format

--- includes/frontend/blocks/build/post-count/index.asset.php
+++ includes/frontend/blocks/build/post-count/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b1ac8d2427f965d281ae');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '808ebf1ecf36bb92d706');
