4cfb62d77835e4ef8710bbb203d1a486d14bdda5
Ajay D'Souza <ajaydsouza@users.noreply.github.com>
2026-06-13 20:26:37 +0400

    sync: update Settings API framework from canonical (unique_field repeater support, sensitive-field sanitization hardening, @package cleanup)

--- includes/admin/settings/class-settings-api.php
+++ includes/admin/settings/class-settings-api.php
@@ -5,7 +5,7 @@
  * Functions to register, read, write and update settings.
  * Portions of this code have been inspired by Easy Digital Downloads, WordPress Settings Sandbox, WordPress Settings API class, etc.
  *
- * @package    WebberZone\Top_Ten
+ * @package WebberZone\Top_Ten
  */
 
 namespace WebberZone\Top_Ten\Admin\Settings;
